2eb3925b6b5dae5b470faee99f44cad7f4d8510e
jalimpia <49337420+jalimpia@users.noreply.github.com>
2020-06-12 17:57:26 +0800

    updata db config

--- model/config.js
+++ model/config.js
@@ -1,22 +1,22 @@
 var mysql = require('mysql');
 
- // var connection = mysql.createPool({
- //     host     : 'localhost',
- //     user     : 'root',
- //     password : '!p@ssword1234',
- //     database : 'tia_db',
- //     multipleStatements: true,
- //     connectionLimit : 10
- // });
+ var connection = mysql.createPool({
+     host     : 'localhost',
+     user     : 'root',
+     password : '!p@ssword1234',
+     database : 'tia_db',
+     multipleStatements: true,
+     connectionLimit : 10
+ });
 
-var connection = mysql.createPool({
-   connectionLimit : 10,
-   host     : 'localhost',
-   user     : 'root',
-   password : '',
-   database : 'tia_db',
-   multipleStatements: true
-});
+// var connection = mysql.createPool({
+//    connectionLimit : 10,
+//    host     : 'localhost',
+//    user     : 'root',
+//    password : '',
+//    database : 'tia_db',
+//    multipleStatements: true
+// });
 
 connection.getConnection(function(err) {
     if (err){
